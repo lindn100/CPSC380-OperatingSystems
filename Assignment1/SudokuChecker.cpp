e063c08775c3f18496ce45dacc5e6e7eb2991f06
Ryan Lindner <42722107+lindn100@users.noreply.github.com>
2019-03-19 18:17:47 -0700

    bug - identifying squares

--- Assignment1/SudokuChecker.cpp
+++ Assignment1/SudokuChecker.cpp
@@ -57,18 +57,32 @@ void SudokuChecker::printBoard(vector<vector<int>> board) {
 }
 
 void SudokuChecker::runProgram() {
-  vScan(board);
-  hScan(board);
-  sScan(board);
+  vector<int> v = vScan(board);
+  vector<int> h = hScan(board);
+  vector<int> s = sScan(board);
+
+  if(v.size() == 0 && h.size() == 0 && s.size() == 0) {
+    cout << "Valid sudoku board. Exiting." << endl;
+  } else {
+    for(int i = 0; i < v.size(); i += 3) {
+      cout << "Duplicate value of " << v[i] << " found at " << h[i+1] << "," << v[i+1] << " in square " << s[i+1] << ". This value should be " << v[i+2];
+    }
+  }
 }
 
-void SudokuChecker::vScan(vector<vector<int>> board) {
+vector<int> SudokuChecker::vScan(vector<vector<int>> board) {
+  vector<int> invalids;
+  vector<int> dups;
+  vector<int> col;
+  vector<int> missing;
   for(int i = 0; i < board.size(); ++i) {
     vector<int> seen(9);
     for(int j = 0; j < board[0].size(); ++j) {
       if(seen[board[j][i]-1] != 0) {
         //problem occurrs here
-        cout << "Duplicate vertical number " << board[j][i] << " found in column " << i+1 << endl;
+        //cout << "Duplicate vertical number " << board[j][i] << " found in column " << i+1 << endl;
+        dups.push_back(board[j][i]);
+        col.push_back(i+1);
       } else {
         seen[board[j][i]-1] = board[j][i];
       }
@@ -76,19 +90,33 @@ void SudokuChecker::vScan(vector<vector<int>> board) {
     }
     for(int k = 0; k < 9; ++k) {
       if(seen[k] != k+1) {
-        cout << "Missing number " << k+1 << " in column " << i+1 << endl;
+        //cout << "Missing number " << k+1 << " in column " << i+1 << endl;
+        missing.push_back(k+1);
       }
     }
   }
+   for(int i = 0; i < dups.size(); ++i) {
+     invalids.push_back(dups[i]); //order of the returning array will be duplicated num followed by missing num
+     invalids.push_back(col[i]);
+     invalids.push_back(missing[i]);
+   }
+
+   return invalids;
 }
 
-void SudokuChecker::hScan(vector<vector<int>> board) {
+vector<int> SudokuChecker::hScan(vector<vector<int>> board) {
+  vector<int> invalids;
+  vector<int> dups;
+  vector<int> row;
+  vector<int> missing;
   for(int i = 0; i < board.size(); ++i) {
     vector<int> seen(9);
     for(int j = 0; j < board[0].size(); ++j) {
       if(seen[board[i][j]-1] != 0) {
         //problem occurrs here
-        cout << "Duplicate horizontal number " << board[i][j] << " found in row " << i+1 << endl;
+        //cout << "Duplicate horizontal number " << board[i][j] << " found in row " << i+1 << endl;
+        dups.push_back(board[i][j]);
+        row.push_back(i+1);
       } else {
         seen[board[i][j]-1] = board[i][j];
       }
@@ -96,13 +124,27 @@ void SudokuChecker::hScan(vector<vector<int>> board) {
     }
     for(int k = 0; k < 9; ++k) {
       if(seen[k] != k+1) {
-        cout << "Missing number " << k+1 << " in row " << i+1 << endl;
+        //cout << "Missing number " << k+1 << " in row " << i+1 << endl;
+        missing.push_back(k+1);
       }
     }
   }
+
+  for(int i = 0; i < dups.size(); ++i) {
+    invalids.push_back(dups[i]);
+    invalids.push_back(row[i]);
+    invalids.push_back(missing[i]);
+  }
+
+  return invalids;
 }
 
-void SudokuChecker::sScan(vector<vector<int>> board) {
+vector<int> SudokuChecker::sScan(vector<vector<int>> board) {
+
+  vector<int> invalids;
+  vector<int> dups;
+  vector<int> square;
+  vector<int> missing;
 
   for(int a = 0; a < 3; ++a) {
     for(int b = 0; b < 3; ++b) {
@@ -111,6 +153,8 @@ void SudokuChecker::sScan(vector<vector<int>> board) {
         for(int j = b*3; j < (b*3)+3; ++j) {
           if(seen[board[i][j]-1] != 0) {
             cout << "Duplicate number " << board[i][j] << " in square " << a+b+1 << endl;
+            dups.push_back(board[i][j]);
+            square.push_back(a+b+1);
           } else {
             seen[board[i][j]-1] = board[i][j];
           }
@@ -118,9 +162,18 @@ void SudokuChecker::sScan(vector<vector<int>> board) {
       }
       for(int k = 0; k < 9; ++k) {
         if(seen[k] != k+1) {
-          cout << "Missing number " << k+1 << " in square " << a+b+1 << endl;
+          //cout << "Missing number " << k+1 << " in square " << a+b+1 << endl;
+          missing.push_back(k+1);
         }
       }
     }
   }
+
+  for(int i = 0; i < dups.size(); ++i) {
+    invalids.push_back(dups[i]);
+    invalids.push_back(square[i]);
+    invalids.push_back(missing[i]);
+  }
+
+  return invalids;
 }
--- Assignment1/SudokuChecker.h
+++ Assignment1/SudokuChecker.h
@@ -16,9 +16,9 @@ public:
   void loadFile(string);
   void printBoard(vector<vector<int>> board);
   void runProgram();
-  void vScan(vector<vector<int>> board);
-  void hScan(vector<vector<int>> board);
-  void sScan(vector<vector<int>> board);
+  vector<int> vScan(vector<vector<int>> board);
+  vector<int> hScan(vector<vector<int>> board);
+  vector<int> sScan(vector<vector<int>> board);
 
 private:
   string fileName;
