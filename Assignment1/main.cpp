f974589f06a5a5c1acf2628d6401a84a7fa9e280
Ryan Lindner <42722107+lindn100@users.noreply.github.com>
2019-03-22 22:48:59 -0700

    Delete main.cpp

--- Assignment1/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "CommandLineChecker.h"
-#include "SudokuChecker.h"
-#include <pthread.h>
-
-int main(int argc, char* argv[]) {
-  CommandLineChecker checkIt(argc);
-  SudokuChecker a1(argv[1]);
-  return 0;
-}
