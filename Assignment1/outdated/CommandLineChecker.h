710f86a8956e3e4cbce62777cbe824478715fd37
Ryan Lindner <42722107+lindn100@users.noreply.github.com>
2019-03-22 22:45:55 -0700

    Create CommandLineChecker.h

---
+++ Assignment1/outdated/CommandLineChecker.h
@@ -0,0 +1,12 @@
+#ifndef CommandLineChecker_H
+#define CommandLineChecker_H
+
+class CommandLineChecker
+{
+public:
+  CommandLineChecker(int); //makes sure that argc is exactly 2
+  ~CommandLineChecker();
+
+private:
+};
+#endif
