a1965e89f456751144a7c4ec3747508554ac482f
Ryan Lindner <42722107+lindn100@users.noreply.github.com>
2019-05-01 18:51:17 -0700

    Add files via upload

--- Assignment3/RMS.cpp
+++ Assignment3/RMS.cpp
@@ -1,3 +1,8 @@
+/*	QUESTIONS
+2. Priorities of threads
+3. C++ implementation of binary semaphore instead of syntax
+
+*/
 #include <iostream>
 #include <pthread.h>
 #include <sched.h>
@@ -12,7 +17,12 @@ int count2 = 0;
 int count3 = 0;
 int count4 = 0;
 
-int[][] board = new int[10][10];
+int miss1 = 0;
+int miss2 = 0;
+int miss3 = 0;
+int miss4 = 0;
+
+vector<vector<int>> board(10, vector<int> (10, 1));
 
 
 mutex mtx1; //using mutex's to function as a binary semaphore
@@ -20,23 +30,32 @@ mutex mtx2;
 mutex mtx3;
 mutex mtx4;
 
-void *scheduluer(void *);
+mutex boolmtx1;
+mutex boolmtx2;
+mutex boolmtx3;
+mutex boolmtx4;
+
+bool working1 = false; //bools used to see if the thread is still doing work when scheduled again
+bool working2 = false;
+bool working3 = false;
+bool working4 = false;
+
+void *scheduler(void *);
 void *superfast(void *);
 void *fast(void *);
 void *medium(void *);
 void *slow(void *);
 
 int main() {
-	for(int i = 0; i < 10; ++i) {
-		for(int j = 0; j < 10; ++j) {
-			board[i][j] = 1;
-		}
-	}
-
 	mtx1.lock();
 	mtx2.lock();
 	mtx3.lock();
 	mtx4.lock();
+
+	boolmtx1.unlock();
+	boolmtx2.unlock();
+	boolmtx3.unlock();
+	boolmtx4.unlock();
 
 
 	pthread_t myThreads[5];
@@ -73,19 +92,48 @@ void* scheduler(void *) {
 	while(counter < 10) {
 		++counter;
 		for(int i = 1; i < 17; ++i) {
+			boolmtx1.lock();
+			if(working1) {
+				miss1++;
+			}
+			boolmtx1.unlock();
 			mtx1.unlock();
-			if(i % 4 == 0) {
+			if(i % 2 == 0) {
+				boolmtx2.lock();
+				if(working2) {
+					miss2++;
+				}
+				boolmtx2.unlock();
 				mtx2.unlock();
 			}
-			if(i % 8 == 0) {
+			if(i % 4 == 0) {
+				boolmtx3.lock();
+				if(working3) {
+					miss3++;
+				}
+				boolmtx3.unlock();
 				mtx3.unlock();
 			}
 			if(i % 16 == 0) {
+				boolmtx4.lock();
+				if(working4) {
+					miss4++;
+				}
+				boolmtx4.unlock();
 				mtx4.unlock();
 			}
-			sleep(1);
+		usleep(10000);
 		}
 	}
+	cout << "Super Fast ran: " << count1 << "times" << endl;
+	cout << "Fast ran: " << count2 << "times" << endl;
+	cout << "Medium ran: " << count3 << "times" << endl;
+	cout << "Slow ran: " << count4 << "times" << endl;
+	cout << "Super Fast missed: " << miss1 << "times" << endl;
+	cout << "Fast missed: " << miss2 << "times" << endl;
+	cout << "Medium missed: " << miss3 << "times" << endl;
+	cout << "Slow missed: " << miss4 << "times" << endl;
+	exit(0);
 }
 
 void doWork() {
@@ -103,21 +151,60 @@ void doWork() {
 }
 
 void *superfast(void *) {
-	mtx1.lock();
-	doWork();
+	while(true) {
+		mtx1.lock();
+		count1++;
+		boolmtx1.lock(); //using a mutex to lock the critical section of the bool
+		working1 = true;
+		boolmtx1.unlock();
+		doWork();
+		boolmtx1.lock();
+		working1 = false;
+		boolmtx1.unlock();
+	}
 }
 
 void *fast(void *) {
-	mtx2.lock();
-	doWork();
+	while(true) {
+		mtx2.lock();
+		count2++;
+		boolmtx2.lock();
+		working2 = true;
+		boolmtx2.unlock();
+//causes overrun		for(int i = 0; i < 100000; ++i) 
+			doWork();
+
+		boolmtx2.lock();
+		working2 = false;
+		boolmtx2.unlock();
+	}
 }
 
 void *medium(void *) {
-	mtx3.lock();
-	doWork();
+	while(true) {
+		mtx3.lock();
+		count3++;
+		boolmtx3.lock();
+		working3 = true;
+		boolmtx3.unlock();
+//causes overrun + seg fault		for (int i = 0; i < 100000; ++i) 
+			doWork();
+		boolmtx3.lock();
+		working3 = false;
+		boolmtx3.unlock();
+	}
 }
 
 void *slow(void *) {
-	mtx4.lock():
-	doWork();
+	while(true) {
+		mtx4.lock();
+		count4++;
+		boolmtx4.lock();
+		working4 = true;
+		boolmtx4.unlock();
+		doWork();
+		boolmtx4.lock();
+		working4 = false;
+		boolmtx4.unlock();
+	}
 }
