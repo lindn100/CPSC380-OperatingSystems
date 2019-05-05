c880dea653a6f0cd6afedeb2d2ba7f3d8292aa7b
Ryan Lindner <42722107+lindn100@users.noreply.github.com>
2019-05-05 13:46:55 -0700

    needs processor affinity

--- Assignment3/RMS.cpp
+++ Assignment3/RMS.cpp
@@ -9,6 +9,7 @@
 #include <unistd.h> //for usleep
 #include <mutex>
 #include <vector>
+#include <semaphore.h>
 
 using namespace std;
 
@@ -23,6 +24,16 @@ int miss3 = 0;
 int miss4 = 0;
 
 vector<vector<int>> board(10, vector<int> (10, 1));
+
+sem_t s1;
+sem_t s2;
+sem_t s3;
+sem_t s4;
+
+sem_t bools1;
+sem_t bools2;
+sem_t bools3;
+sem_t bools4;
 
 
 mutex mtx1; //using mutex's to function as a binary semaphore
@@ -47,6 +58,26 @@ void *medium(void *);
 void *slow(void *);
 
 int main() {
+	struct sched_param params;
+
+/*	
+
+	cpu_set_t mask;
+	int status;
+
+	CPU_ZERO(&mask);
+	CPU_SET(cpuAffinity, &mask);
+*/
+	sem_init(&s1, 0, 0);
+	sem_init(&s2, 0, 0);
+	sem_init(&s3, 0, 0);
+	sem_init(&s4, 0, 0);
+
+	sem_init(&bools1, 0, 1);
+	sem_init(&bools2, 0, 1);
+	sem_init(&bools3, 0, 1);
+	sem_init(&bools4, 0, 1);
+
 	mtx1.lock();
 	mtx2.lock();
 	mtx3.lock();
@@ -57,24 +88,37 @@ int main() {
 	boolmtx3.unlock();
 	boolmtx4.unlock();
 
+	params.sched_priority = sched_get_priority_max(SCHED_FIFO);
+
 
 	pthread_t myThreads[5];
 	for(int i = 0; i < 5; ++i) {
 		switch(i) {
 			case 0:
 				pthread_create(&myThreads[0], NULL, superfast, NULL);
+				//pthread_setaffinity_np(threads[0], sizeof(cpu_set_t), &cpuset);
+				params.sched_priority--;
+				pthread_setschedparam(myThreads[0], SCHED_FIFO, &params);
 				break;
 			case 1:
 				pthread_create(&myThreads[1], NULL, fast, NULL);
+				params.sched_priority--;
+				pthread_setschedparam(myThreads[1], SCHED_FIFO, &params);
 				break;
 			case 2:
 				pthread_create(&myThreads[2], NULL, medium, NULL);
+				params.sched_priority--;
+				pthread_setschedparam(myThreads[2], SCHED_FIFO, &params);
 				break;
 			case 3:
 				pthread_create(&myThreads[3], NULL, slow, NULL);
+				params.sched_priority--;
+				pthread_setschedparam(myThreads[3], SCHED_FIFO, &params);
 				break;
 			case 4:
 				pthread_create(&myThreads[4], NULL, scheduler, NULL);
+				params.sched_priority = sched_get_priority_max(SCHED_FIFO);
+				pthread_setschedparam(myThreads[4], SCHED_FIFO, &params);
 				break;
 		}
 
@@ -83,7 +127,6 @@ int main() {
 	for (int i = 0; i < 5; ++i) {
 		pthread_join(myThreads[i], NULL);
 	}
-
 	return 0;
 }
 
@@ -92,35 +135,35 @@ void* scheduler(void *) {
 	while(counter < 10) {
 		++counter;
 		for(int i = 1; i < 17; ++i) {
-			boolmtx1.lock();
+			sem_wait(&bools1);//boolmtx1.lock();
 			if(working1) {
 				miss1++;
 			}
-			boolmtx1.unlock();
-			mtx1.unlock();
+			sem_post(&bools1);//boolmtx1.unlock();
+			sem_post(&s1);//mtx1.unlock();
 			if(i % 2 == 0) {
-				boolmtx2.lock();
+				sem_wait(&bools2);//boolmtx2.lock();
 				if(working2) {
 					miss2++;
 				}
-				boolmtx2.unlock();
-				mtx2.unlock();
+				sem_post(&bools2);//boolmtx2.unlock();
+				sem_post(&s2);//mtx2.unlock();
 			}
 			if(i % 4 == 0) {
-				boolmtx3.lock();
+				sem_wait(&bools3);//boolmtx3.lock();
 				if(working3) {
 					miss3++;
 				}
-				boolmtx3.unlock();
-				mtx3.unlock();
+				sem_post(&bools3);//boolmtx3.unlock();
+				sem_post(&s3);//mtx3.unlock();
 			}
 			if(i % 16 == 0) {
-				boolmtx4.lock();
+				sem_wait(&bools4);//boolmtx4.lock();
 				if(working4) {
 					miss4++;
 				}
-				boolmtx4.unlock();
-				mtx4.unlock();
+				sem_post(&bools4);//boolmtx4.unlock();
+				sem_post(&s4);//mtx4.unlock();
 			}
 		usleep(10000);
 		}
@@ -152,59 +195,59 @@ void doWork() {
 
 void *superfast(void *) {
 	while(true) {
-		mtx1.lock();
+		sem_wait(&s1);//mtx1.lock();
 		count1++;
-		boolmtx1.lock(); //using a mutex to lock the critical section of the bool
+		sem_wait(&bools1);//boolmtx1.lock(); //using a mutex to lock the critical section of the bool
 		working1 = true;
-		boolmtx1.unlock();
+		sem_post(&bools1);//boolmtx1.unlock();
 		doWork();
-		boolmtx1.lock();
+		sem_wait(&bools1);//boolmtx1.lock();
 		working1 = false;
-		boolmtx1.unlock();
+		sem_post(&bools1);//boolmtx1.unlock();
 	}
 }
 
 void *fast(void *) {
 	while(true) {
-		mtx2.lock();
+		sem_wait(&s2);//mtx2.lock();
 		count2++;
-		boolmtx2.lock();
+		sem_wait(&bools2);//boolmtx2.lock();
 		working2 = true;
-		boolmtx2.unlock();
-//causes overrun		for(int i = 0; i < 100000; ++i) 
+		sem_post(&bools2);//boolmtx2.unlock();
+//		for(int i = 0; i < 100000; ++i) 
 			doWork();
 
-		boolmtx2.lock();
+		sem_wait(&bools2);//boolmtx2.lock();
 		working2 = false;
-		boolmtx2.unlock();
+		sem_post(&bools2);boolmtx2.unlock();
 	}
 }
 
 void *medium(void *) {
 	while(true) {
-		mtx3.lock();
+		sem_wait(&s3);//mtx3.lock();
 		count3++;
-		boolmtx3.lock();
+		sem_wait(&bools3);//boolmtx3.lock();
 		working3 = true;
-		boolmtx3.unlock();
-//causes overrun + seg fault		for (int i = 0; i < 100000; ++i) 
+		sem_post(&bools3);//boolmtx3.unlock();
+		for (int i = 0; i < 100000; ++i) 
 			doWork();
-		boolmtx3.lock();
+		sem_wait(&bools3);//boolmtx3.lock();
 		working3 = false;
-		boolmtx3.unlock();
+		sem_post(&bools3);//boolmtx3.unlock();
 	}
 }
 
 void *slow(void *) {
 	while(true) {
-		mtx4.lock();
+		sem_wait(&s4);//mtx4.lock();
 		count4++;
-		boolmtx4.lock();
+		sem_wait(&bools4);//boolmtx4.lock();
 		working4 = true;
-		boolmtx4.unlock();
+		sem_post(&bools4);//boolmtx4.unlock();
 		doWork();
-		boolmtx4.lock();
+		sem_wait(&bools4);//boolmtx4.lock();
 		working4 = false;
-		boolmtx4.unlock();
+		sem_post(&bools4);//boolmtx4.unlock();
 	}
 }
